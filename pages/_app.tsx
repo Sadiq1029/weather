a1a07e260f9fa020e865795a19642178457b67b3
Sadiqur Rahman <sadiqurrahman9m05@gmail.com>
2022-06-28 09:48:49 +0600

    added service worker

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,26 @@ import type { AppProps } from "next/app";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import dataReducer from "../states/data";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+        navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log(
+              "Service Worker registration successful with scope: ",
+              registration.scope
+            );
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+  }, []);
   const store = configureStore({
     reducer: {
       data: dataReducer,
