57373c02b8db4737e59fcf4dd643aaf4de113698
Sadiqur Rahman <sadiqurrahman9m05@gmail.com>
2022-06-29 10:58:15 +0600

    added fetch listener

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,11 @@
 self.addEventListener('install', (e) => {
     console.log('service worker installed')
 })
+
+self.addEventListener('activate', () => {
+    console.log('service worker activate')
+})
+
+self.addEventListener('fetch', (e) => {
+    console.log(e)
+})
