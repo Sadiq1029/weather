b1a437b7ff446095cf7d7651795fef6cca395f1f
Sadiqur Rahman <sadiqurrahman9m05@gmail.com>
2022-06-30 08:39:44 +0600

    added charset

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,12 @@ export default function Document() {
         <link rel="apple-touch-icon" href="/icons/icon-128x128.png" />
         <meta name="apple-mobile-web-app-status-bar" content="#fff" />
         <meta name="theme-color" content="#f2f2f2" />
+        <link
+          rel="shortcut icon"
+          href="/icons/icon-128x128.png"
+          type="image/x-icon"
+        />
+        <meta charSet="UTF-8"></meta>
       </Head>
       <body>
         <Main />
